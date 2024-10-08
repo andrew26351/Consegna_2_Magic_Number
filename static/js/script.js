1a9df66f80ffcfbce1ef4e8085f7d2202c2951e6
andrew2635 <andrewleone2007@gmail.com>
2024-10-08 09:53:12 +0200

    modificata la logistica del gioco

--- static/js/script.js
+++ static/js/script.js
@@ -1,49 +1,44 @@
-function getRandomInt(max){
-    return Math.floor(Math.random() * max)
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
 function sanitize(input) {
-    let sanitizedInput = parseInt(input)
+    let sanitizedInput = parseInt(input);
     if (isNaN(sanitizedInput)) {
-        return null
+        return null;
     } else {
-        return sanitizedInput
+        return sanitizedInput;
     }
 }
 
+const max_vite = 5;
+let vite = 0;
+const r = getRandomInt(100);
 
-const max_vite = 5
-let vite = 0
+let userField = document.querySelector("#guess");
+let btn = document.querySelector("#guessBtn");
 
-console.log("Benvenuto nel gioco INDOVINA IL NUMERO!")
-console.log("Il gioco consinste nel indovinare un numero tra 0-99 che io penserò, hai 5 vite")
-console.log("Incominciamo!")
+function handleClick() {
+    let num = sanitize(userField.value);
 
-
-const r = getRandomInt(100)
-//console.log(r)
-
-
-while(vite < max_vite){
-    let num = prompt("Inserisci il numero (0-99)")
-    num = sanitize(num)
-    
-    if (num === null) {
-        console.log("Per favore, inserisci un numero valido.")
-    } else if (num === r) {
-        console.log("Bravo, hai vinto")
-        break
-    } else {
-        vite++
-        if (num < r) {
-            console.log("Troppo piccolo")
+    if (vite < max_vite) {
+        if (num === null) {
+            document.querySelector('#output').innerHTML = "Per favore, inserisci un numero valido.";
+        } else if (num === r) {
+            document.querySelector('#output').innerHTML = "Bravo, hai vinto!";
         } else {
-            console.log("Troppo grande")
+            vite++;
+            if (num < r) {
+                document.querySelector('#output').innerHTML = "Troppo piccolo.";
+            } else {
+                document.querySelector('#output').innerHTML = "Troppo grande.";
+            }
         }
     }
+
+    if (vite === max_vite) {
+        document.querySelector('#output').innerHTML = "Hai esaurito le vite. Il numero era"  + r + ".";
+    }
 }
 
-if (vite === max_vite) {
-    console.log("Hai esaurito le vite. Il numero era " + r)
-}
-
+btn.addEventListener("click", handleClick);
