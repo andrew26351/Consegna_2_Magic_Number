a5eadce3ffd84643c416c98d597277bf806562d5
andrew2635 <andrewleone2007@gmail.com>
2024-10-22 09:40:54 +0200

    aggiunta transition colori in base alle vite

--- static/js/script.js
+++ static/js/script.js
@@ -9,28 +9,48 @@ const guessNumber = getRandomInt(0, 100);
 
 let btn = document.querySelector("#guessBtn");
 let output = document.querySelector('#output');
+let el = document.querySelector("#element")
 output.innerHTML = `Hai ancora <strong> ${vite} </strong> vite`;
 
-function handleClick() {
+function handleClick(){
     const userField = document.querySelector("#guess");
     const userNumber = parseInt(userField.value);
 
+
     if (vite > 0) {
-        if (isNaN(userNumber) || userNumber < 0 || userNumber > 100) {
+        if (isNaN(userNumber) || userNumber < 0 || userNumber > 100){
             output.innerHTML = "Inserisci un numero intero da 1 a 100.";
-        } else if (userNumber === guessNumber) {
+        } else if (userNumber === guessNumber){
             output.innerHTML = "Bravo, hai indovinato!";
-        } else {
+        } else{
             vite--;
-            if (userNumber > guessNumber) {
+            if (userNumber > guessNumber){
                 output.innerHTML = "Troppo grande. ";
-            } else if (userNumber < guessNumber) {
+            } else if (userNumber < guessNumber){
                 output.innerHTML = "Troppo piccolo. ";
             }
+            
+            if (vite <= 3){
+                el.classList.remove("greenBg")
+                el.classList.add("yellowBg")
+
+                btn.classList.remove("greenBgBtn")
+                btn.classList.add("yellowBgBtn")
+
+                el.classList.add("transition")
+                btn.classList.add("transition")
+            }
+            if (vite < 2){
+                el.classList.remove("yellowBg")
+                el.classList.add("redBg")
+
+                btn.classList.remove("yellowBgBtn")
+                btn.classList.add("redBgBtn")
+            }   
             output.innerHTML += `Hai ancora <strong>${vite}</strong> vite.`;
         }
 
-        if (vite === 0) {
+        if (vite === 0){
             output.innerHTML = `Hai esaurito le vite. Il numero era ${guessNumber}.`;
         }
     }
